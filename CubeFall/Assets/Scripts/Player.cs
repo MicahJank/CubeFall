7464881f5fe940502cbc361044cabf6131231108
MicahJank <MicahJank@users.noreply.github.com>
2019-01-14 17:21:35 -0600

    ProjCubeFall 0.05
    Implemented basic level design. Adjusted character movement and rotation

--- CubeFall/Assets/Scripts/Player.cs
+++ CubeFall/Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     AudioSource jumpSound;
 
     [SerializeField] private float _jumpForce = 10f;
+    [SerializeField] private float cubeRotation = 100f;
 
     // Will determine if the player can rotate left or right
     enum Rotation { Left, Right, noRotation };
@@ -20,41 +21,58 @@ public class Player : MonoBehaviour
     {
         jumpSound = GetComponent<AudioSource>();
         rigidBody = GetComponent<Rigidbody>();
-        rotationState = Rotation.noRotation;
-        
+        rotationState = Rotation.noRotation;     
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckPlayerInput();
+        Rotate();
+        Jumping();
     }
 
-    private void CheckPlayerInput()
+    private void Rotate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            jumpSound.Play();
-            rigidBody.AddRelativeForce(Vector3.up * _jumpForce, ForceMode.VelocityChange); // VelocityChange so the force ignores mass
-        }
+        float rotationOnFrame = cubeRotation * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A) && (rotationState != Rotation.Right))
         {
             rotationState = Rotation.Left;
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationOnFrame, Space.World);
         }
         else if (Input.GetKey(KeyCode.D) && (rotationState != Rotation.Left))
         {
             rotationState = Rotation.Right;
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationOnFrame, Space.World);
         }
         else
         {
             rotationState = Rotation.noRotation;
-           
         }
-       
+    }
 
+    private void Jumping() // TODO make better Jump - longer spacebar held the higher the jump
+    {
+        float jumpSpeedOnFrame = _jumpForce * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.velocity = new Vector3(0f, 0f, 0f);
+            //rigidBody.freezeRotation;
 
+            jumpSound.Play();
+            rigidBody.AddRelativeForce(Vector3.up * jumpSpeedOnFrame, ForceMode.VelocityChange); // VelocityChange so the force ignores mass
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly": Debug.Log("OK");
+                break;
+
+            default: Debug.Log("Dead"); // TODO kill player
+                break;
+        }
     }
 }
