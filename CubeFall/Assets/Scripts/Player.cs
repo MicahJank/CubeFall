a282e61ab97d3535674c39b3a249c25fc2293e86
MicahJank <MicahJank@users.noreply.github.com>
2019-01-15 19:21:04 -0600

    ProjCubeJump 0.08
    Implemented level success sound and death sound.

--- CubeFall/Assets/Scripts/Player.cs
+++ CubeFall/Assets/Scripts/Player.cs
@@ -5,7 +5,14 @@ public class Player : MonoBehaviour
 {
     Rigidbody rigidBody;
 
-    AudioSource jumpSound;
+    AudioSource audioSource;
+
+    [SerializeField] AudioClip jumpSound;
+    [SerializeField] AudioClip preDeathSound;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip beatLevelSound;
+
+
 
     [SerializeField] private float _jumpForce = 10f;
     [SerializeField] private float cubeRotation = 100f;
@@ -14,10 +21,13 @@ public class Player : MonoBehaviour
     enum Rotation { Left, Right, noRotation };
     Rotation rotationState;
 
+    enum State { Alive, Dying, lvlSwitching };
+    State playerState = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
-        jumpSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
         rigidBody = GetComponent<Rigidbody>();
         rotationState = Rotation.noRotation;     
     }
@@ -25,8 +35,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rotate();
-        Jumping();
+        if (playerState == State.Alive)
+        {
+            Rotate();
+            Jumping();
+        }
+       
     }
 
     private void Rotate()
@@ -54,28 +68,55 @@ public class Player : MonoBehaviour
         float jumpSpeedOnFrame = _jumpForce * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidBody.velocity = new Vector3(0f, 0f, 0f);
-            //rigidBody.freezeRotation;
-
-            jumpSound.Play();
+            rigidBody.velocity = new Vector3(0f, 0f, 0f);          
+            audioSource.PlayOneShot(jumpSound);
             rigidBody.AddRelativeForce(Vector3.up * jumpSpeedOnFrame, ForceMode.VelocityChange); // VelocityChange so the force ignores mass
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        if (playerState != State.Alive) { return; } // ignore collisons on death
+ 
         switch (other.gameObject.tag)
         {
             case "Friendly":
                 break;
 
-            case "Finish": Debug.Log("Hit Finish");
-                SceneManager.LoadScene(1);
+            case "Finish":
+                Win();
                 break;
 
-            default: Debug.Log("Dead"); // TODO kill player
-                SceneManager.LoadScene(0);
+            default:
+                Die();
                 break;
         }
+    }
+
+    private void Win()
+    {
+        playerState = State.lvlSwitching;
+        audioSource.Stop();
+        audioSource.PlayOneShot(beatLevelSound);
+        Invoke("LoadNextLevel", 3f); // Invoke is a lesser form of CoRoutines
+    }
+
+    private void Die()
+    {
+        Debug.Log("Dead"); // TODO kill player
+        playerState = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound); // TODO predeath sound needs to play before this       
+        Invoke("LoadFirstLevel", 2f);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); // TODO allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 }
